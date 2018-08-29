abf6faf787562ed5a7472dfe6f9107a3d079508a
nizamnizu <39334941+nizamnizu@users.noreply.github.com>
2018-08-29 15:45:10 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-function resizeHeaderOnScroll() {
-  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
-  shrinkOn = 200,
-  headerEl = document.getElementById('js-header');
-
-  if (distanceY > shrinkOn) {
-    headerEl.classList.add("smaller");
-  } else {
-    headerEl.classList.remove("smaller");
-  }
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.addEventListener('scroll', resizeHeaderOnScroll);
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
